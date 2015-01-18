d4c3e64dd4d752d717e345bee299d86a1425670d
Varun Hooda <hoodavarun2050@gmail.com>
2015-01-19 00:12:15 +0530

    hockey landing page

--- cappuccino/client/app/guest/guest.js
+++ cappuccino/client/app/guest/guest.js
@@ -7,5 +7,10 @@ angular.module('cappuccinoApp')
         url: '/',
         templateUrl: 'app/guest/guest.html',
         controller: 'GuestCtrl'
+      })
+      .state('guest-signup', {
+        url: '/guest-signup',
+        templateUrl: 'app/guest/visitorsignup.html',
+        controller: 'GuestCtrl'
       });
   });
